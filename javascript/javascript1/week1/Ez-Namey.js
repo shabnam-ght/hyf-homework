6ae244871bf4beb8317288bb194ff06e43b0355d
shabnam-ght <www.shabnamght77@gmail.com>
2021-06-30 18:58:48 +0200

    added Ez-Namey.js

---
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -0,0 +1,8 @@
+let firstWords = ["big", "beautiful", "amazing", "small", "nice", "ugly", "delicious", "red", "awsome", "lovely"];
+
+let secondWords = ["corprate", "institute", "company", "shop", "market", "application", "website", "restaurant ", "coffeeshop", "business"];
+
+let randomNumber = Math.floor(Math.random() * 10);
+let randomNumber2 = Math.floor(Math.random() * 10);
+let startupName = firstWords[randomNumber] + " " + secondWords[randomNumber2];
+console.log(startupName);
