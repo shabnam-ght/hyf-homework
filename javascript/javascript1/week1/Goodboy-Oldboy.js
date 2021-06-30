74d7bb0df11a2a722ed8156ae42df2a02e769f7f
shabnam-ght <www.shabnamght77@gmail.com>
2021-06-30 18:59:20 +0200

    added Goodboy-Oldboy.js

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,10 @@
+let dogYearOfBirth = 2015;
+let dogYearFuture = 2045;
+let dogYear = (dogYearFuture - dogYearOfBirth) * 7;
+let inHumanYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears) {
+    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture)
+} else {
+    console.log("Your dog will be " + inHumanYear + " human years old in " + dogYearFuture)
+}
