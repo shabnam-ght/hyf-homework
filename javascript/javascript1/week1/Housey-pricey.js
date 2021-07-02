4bee9fcf61a70cf5ef7f67a1a30a1c800c05ac82
shabnam-ght <www.shabnamght77@gmail.com>
2021-07-02 21:53:27 +0200

    changed let to const

--- javascript/javascript1/week1/Housey-pricey.js
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -1,12 +1,12 @@
-let peter = {
+const peter = {
     houseWidth: 8,
     houseDepth: 10,
     houseHight: 10,
     gardenSizeInM2: 100,
     houseCost: 2500000
 }
-let volumeInMetersPeter = peter.houseWidth * peter.houseDepth * peter.houseHight;
-let housePricePeter = volumeInMetersPeter * 2.5 * 1000 + peter.gardenSizeInM2 * 300;
+const volumeInMetersPeter = peter.houseWidth * peter.houseDepth * peter.houseHight;
+const housePricePeter = volumeInMetersPeter * 2.5 * 1000 + peter.gardenSizeInM2 * 300;
 if (housePricePeter > peter.houseCost) {
     console.log("peter has payed less money that house value")
 } else if (housePricePeter === peter.houseCost) {
@@ -15,15 +15,15 @@ if (housePricePeter > peter.houseCost) {
 else {
     console.log("peter has payed more money that house value")
 }
-let julia = {
+const julia = {
     houseWidth: 5,
     houseDepth: 11,
     houseHight: 8,
     gardenSizeInM2: 70,
     houseCost: 1000000
 }
-let volumeInMetersJulia = julia.houseWidth * julia.houseDepth * julia.houseHight;
-let housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + julia.gardenSizeInM2 * 300;
+const volumeInMetersJulia = julia.houseWidth * julia.houseDepth * julia.houseHight;
+const housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + julia.gardenSizeInM2 * 300;
 if (housePriceJulia > julia.houseCost) {
     console.log("Julia pays more than the market value of the house.")
 } else if (housePriceJulia === julia.houseCost) {
