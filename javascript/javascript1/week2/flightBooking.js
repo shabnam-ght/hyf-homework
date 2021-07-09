3e7e7f7eb2e3280468e83652f0dff603848de6d9
shabnam-ght <www.shabnamght77@gmail.com>
2021-07-09 10:05:39 +0200

    added flightBooking.js

---
+++ javascript/javascript1/week2/flightBooking.js
@@ -0,0 +1,13 @@
+function getFullname(firstname, surname,useFormalName){
+    let fullName=firstname + ' ' + surname;
+    if (useFormalName===true){
+        fullName='lord'+' '+fullName;
+    }
+    
+    return fullName;
+
+}
+let fullname1 ="shabnam";
+let fullname2="ghatei";
+let bool=true;
+console.log(getFullname(fullname1 ,fullname2 ,bool));
