9ff364a347e4f10a552b8d3c6bb07f6af5df6a3a
shabnam-ght <www.shabnamght77@gmail.com>
2021-08-12 14:16:31 +0200

    made changes

--- javascript/javascript1/week4/js-week4.js
+++ javascript/javascript1/week4/js-week4.js
@@ -1,71 +1,71 @@
-let userName="";
-let toDoList=[];
+const userName ="";
+const toDoList =[];
 function getReply(command){
-    command=command.toLowerCase();
-    let resualt='command is not defined!';
+    command =command.toLowerCase();
+    let response ='command is not defined!';
     
-    if (command.includes("hello my name is ")){
+    if (command.startsWith("hello my name is ")){
         if(userName===''){
             userName=command.substr(17,command.length-1);
             if(userName===''){
-                resualt='please inter your name!';
+                response ='please inter your name!';
             }else{
-                resualt='nice to meet you '+userName;
+                response ='nice to meet you '+userName;
             }
         }else{
-            resualt='we have met before '+userName;
+            response ='we have met before '+userName;
         }
     }else if(command.includes("what is my name")){
         if(userName===''){
-            resualt="we have not met before!";
+            response ="we have not met before!";
         }else{
-            resualt='you are '+userName;
+            response ='you are '+userName;
         }
-    }else if (command.includes("add ") && command.includes(" to my todo")){
-        let todo=command.substr(4,command.length - 15);
+    }else if (command.startsWith("add ") && command.endsWith(" to my todo")){
+        let todo =command.substr(4,command.length - 15);
         toDoList.push(todo);
-        resualt=todo + ' added to your todo!';
-    }else if (command.includes("remove ") && command.includes(" from my todo")){
-        let todo=command.substr(7,command.length - 20);
+        response =todo + ' added to your todo!';
+    }else if (command.startsWith("remove ") && command.endsWith(" from my todo")){
+        let todo =command.substr(7,command.length - 20);
         for(let i=0;i<toDoList.length;i++){
             if(toDoList[i]===todo){
                 toDoList.splice(i,1);
             }
         }
-        resualt=todo + ' removed from your todo!';
+        response =todo + ' removed from your todo!';
     }else if (command.includes("what is on my todo")){
-         resualt=toDoList;
+        response =toDoList;
     }else if (command.includes("what day is it today")){
-        let date=new Date();
-        let day=date.getDay();
-        let digitMonth=date.getMonth();
-        let year=date.getFullYear();
+        let date =new Date();
+        let day =date.getDay();
+        let digitMonth =date.getMonth();
+        let year =date.getFullYear();
         let month = ["January", "February","March", "April","May", "June", "July","August","September","October","November","December"]
-        resualt= day+'. of '+month[digitMonth]+' '+year;
+        response = day+'. of '+month[digitMonth]+' '+year;
     }else if(command.includes("what is ")){
-        let numbers=command.substr(8,command.length);
-        numbers=numbers.split(' ');
+        let numbers =command.substr(8,command.length);
+        numbers =numbers.split(' ');
         if(numbers.length===3){
             if(numbers[1]==='+'){
-                resualt=parseInt(numbers[0])+parseInt(numbers[2]);
+                response = parseInt(numbers[0])+parseInt(numbers[2]);
             }else if(numbers[1]==='-'){
-                resualt=parseInt(numbers[0])-parseInt(numbers[2]);
+                response = parseInt(numbers[0])-parseInt(numbers[2]);
             }else if(numbers[1]==='*'){
-                resualt=parseInt(numbers[0])*parseInt(numbers[2]);
+                response = parseInt(numbers[0])*parseInt(numbers[2]);
             }else if(numbers[1]==='/'){
-                resualt=parseInt(numbers[0])/parseInt(numbers[2]);
+                response = parseInt(numbers[0])/parseInt(numbers[2]);
             }else{
-                resualt=0;
+                response =0;
             }
-            resualt='resualt is:'+resualt;
+            response ='resualt is:'+response ;
         }
 
     }else if (command.includes("set a timer for ")){
-        let minutes=parseInt(command.substr(16,command.lenght-9));
-        let seconds=minutes*60;
+        let minutes =parseInt(command.substr(16,command.lenght-9));
+        let seconds =minutes*60;
         let totalSeconds = 0;
-        let interid=setInterval(setTime, 1000);
-        resualt='timer started:';
+        let interid =setInterval(setTime, 1000);
+        response ='timer started:';
         function setTime() {
             if(totalSeconds>=seconds){
                 clearInterval(interid);
