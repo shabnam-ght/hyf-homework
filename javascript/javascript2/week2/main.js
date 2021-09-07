275d7b29b8863fa3b9920d2a77e8edbd8aae9211
shabnam-ght <www.shabnamght77@gmail.com>
2021-09-07 13:25:46 +0200

    javaScript2 week2

--- javascript/javascript2/week2/main.js
+++ javascript/javascript2/week2/main.js
@@ -1,9 +1,9 @@
 console.log("Script loaded");
 
 const products = getAvailableProducts();
-
+const printResult=document.getElementById('filterList')
 function renderProducts(products) {
-    // implement with foreach
+    printResult.innerText='';
     products.forEach(product => {
         const li = document.createElement('li');
         const getUl = document.getElementById('filterList');
@@ -28,14 +28,12 @@ renderProducts(products);
 //Searching for products
 const searchBar = document.getElementById('searchBar');
 
-searchBar.addEventListener('keydown', function () {
+searchBar.addEventListener('keyup', function () {
     const arr = products.filter(elm => {
         const arrNames = elm.name.toLowerCase();
         return arrNames.includes(searchBar.value);
     });
-    const printResult = document.getElementById('filterList').innerText = ' ';
     renderProducts(arr);
-    return printResult;
 });
 /////////////////////////////////////////////////////////////////
 //Filter products based on max price
@@ -45,9 +43,7 @@ maxPrice.addEventListener('keyup', function () {
     const maximumPrice = products.filter(item => {
         return item.price <= maxPrice.value;
     });
-    const printResult = document.getElementById('filterList').innerText = ' ';
     renderProducts(maximumPrice);
-    return maximumPrice;
 });
 
 // minimum price "Extra"
@@ -58,23 +54,19 @@ minPrice.addEventListener('keyup', function () {
     const minimumPrice = products.filter(item => {
         return item.price >= minPrice.value;
     });
-    const printResult = document.getElementById('filterList').innerText = ' ';
     renderProducts(minimumPrice);
-    return minimumPrice;
+    
 });
 
 // Sort the products - optional
-
-// hi ,Simon .Could you please tell me where is the problem here .
-// result in console perfect , but in DOM 'browser page' = Undifine.
 const sort = document.getElementById('sort')
 
 sort.addEventListener('click',function(){
 const rateArray= products.map(item =>item.rating).sort((a,b) =>b-a)
 console.log(rateArray);
-const printResult = document.getElementById('filterList').innerText = ' ';
+
 renderProducts(rateArray);
-return rateArray;
+
 });
 
 
